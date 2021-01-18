a498e47bc0aea56df4d32323649296af1bc3e1e4
BroDan <BrendanA@users.noreply.github.com>
2021-01-18 12:54:35 +1100

    event nav
    added details register and edit buttons

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 import EventList from "@/views/EventList.vue";
-import EventDetails from "@/views/EventDetails.vue";
+import EventLayout from "@/views/Event/Layout.vue";
+import EventDetails from "@/views/Event/Details.vue";
+import EventRegister from "@/views/Event/Register.vue";
+import EventEdit from "@/views/Event/Edit.vue";
 import About from "@/views/About.vue";
 
 const routes = [
@@ -11,16 +14,34 @@ const routes = [
     props: route => ({ page: parseInt(route.query.page) || 1})
   },
   {
-    path: "/about",
+    path: "/about-us",
     name: "About",
     component: About
   },
   {
     path: '/events/:id',
-    name: 'EventDetails',
+    name: 'EventLayout',
     props: true,
-    component: EventDetails
-  }
+    component: EventLayout,
+    children: [
+      {
+        path: '',
+        name: 'EventDetails',
+        component: EventDetails
+      },
+      {
+        path: 'register',
+        name: 'EventRegister',
+        component: EventRegister
+      },
+      {
+        path: 'edit',
+        name: 'EventEdit',
+        component: EventEdit
+      }
+    ]
+  },
+
 ];
 
 const router = createRouter({
---
+++ src/views/Event/Register.vue
@@ -0,0 +1,11 @@
+<template>
+  <div v-if="event">
+    <h1>Register the event here</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['event']
+}
+</script>
